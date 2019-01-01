fe6065d9aaa6d7a301dbfa37761f9b3012c6ae7a
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2019-01-01 15:50:07 +0200

    preload lazy loading routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router'
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
@@ -17,7 +17,9 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
